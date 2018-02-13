62adc3a9c5461920ec4a74772d3506548c06dfbd
Daniel Bachler <daniel.bachler@comcast.net>
2018-02-13 11:24:06 -0700

    Spell checking complete
    The program now checks for correct spelling, and tells user about found errors.  Next step is correcting users spelling.

--- spell_checker.rb
+++ spell_checker.rb
@@ -6,7 +6,8 @@
 #Spell checks words from user input
 
 #Global vars
-$lexicon = Hash.new()
+$lexicon = Hash.new
+$words = Hash.new
 
 #Populates the lexicon from the given file.  Assumed that the given file
 #is a list of english words
@@ -66,16 +67,84 @@ def initialize_lexicon
   puts "Lexicon initialized"
 end
 
+#Reads the file of words to check
+def to_check to_check
+  #Opens the file to check
+  file = File.open(to_check, 'r')
+  #Reads until the end of the file
+  until file.eof?
+    #Clears punctuation to make checking easier
+    remove_punctuation = /[\?\¿\!\¡\.\;\&\@\%\#\|\,]/
+    #For each line check the spelling of each word
+    file.each_line do |line|
+      line.chomp!.downcase!
+      #cleans punctuation
+      line.gsub!(remove_punctuation, "")
+      line.split.each do |word|
+        $words[word] = false
+      end
+    end
+  end
+end
+
+#Checks the spelling of all words in array
+def spell_check
+  temp_hash = Hash.new
+  $words.each do |word|
+    first_char = ''
+    second_char = ''
+    if word.length > 1
+      first_char = word[0][0]
+      second_char = word[0][1]
+    elsif word.length == 1
+      first_char = word[0][0]
+      second_char = word[0][0]
+    end
+    #puts "First char: #{first_char}\nSecond char: #{second_char}"
+    #Searches lexicon for match
+    match = false
+    $lexicon[first_char][second_char].each do |check|
+      if check == word[0]
+        match = true
+        break;
+      end
+    end
+    temp_hash[word[0]] = match
+  end
+  temp_hash.each do |temp|
+    $words[temp[0]] = temp[1]
+  end
+end
+
+#Prints the words that are spelled wrong
+def feedback
+  puts "Spelling errors"
+  errors = 0
+  $words.each do |word|
+    if word[1] == false
+      puts "#{word[0]}"
+      errors += 1
+    end
+  end
+  if errors == 0
+    puts "No errors found"
+  end
+end
+
 def main_loop
-  if ARGV.length < 1
-    puts "Need to specify lexicon source"
+  if ARGV.length < 2
+    puts "Need to specify lexicon source and file to check"
     exit 4
   end
   #Creates the initial lexicon object
   initialize_lexicon
   #Populates the lexicon object with the words from the given file
   populate_lexicon ARGV[0]
-
+  #Reads input file to words array
+  to_check ARGV[1]
+  #Checks the word hash
+  spell_check
+  feedback
 end
 
 if __FILE__==$0
